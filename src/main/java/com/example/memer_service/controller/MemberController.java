b0f7e95c5bbfe93f7e6a999ead740fb7d846ddb9
dianaseong <dianaseong31@gmail.com>
2025-05-15 15:00:27 +0900

    remove security part

--- src/main/java/com/example/memer_service/controller/MemberController.java
+++ src/main/java/com/example/memer_service/controller/MemberController.java
@@ -10,9 +10,9 @@ import java.util.List;
 import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.jwt.Jwt;
+// import org.springframework.security.access.prepost.PreAuthorize;
+// import org.springframework.security.core.annotation.AuthenticationPrincipal;
+// import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -37,13 +37,13 @@ public class MemberController  {
     }
 
 
-    @PreAuthorize("hasRole('ADMIN')")
+   // @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/members/{id}")
-    public ResponseEntity<MemberDto> view(@AuthenticationPrincipal Jwt jwt, @PathVariable(name = "id") Long id) {
+    public ResponseEntity<MemberDto> view( @PathVariable(name = "id") Long id) {
 
-        String email = jwt.getClaimAsString("email");
+        // String email = jwt.getClaimAsString("email");
 
-        log.info("----------------------------------------- email : {}", email);
+        // log.info("----------------------------------------- email : {}", email);
 
         MemberDto memberDto = memberService.findMember(id);
 
